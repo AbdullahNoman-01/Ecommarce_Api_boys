291778bb3bc7ee4c39f8575f1ead81898575b5d2
AbdullahNoman-01 <abdullahalnoman4343n@gmail.com>
2026-04-18 09:52:39 +0600

    solve cors

--- fassion_dress/settings.py
+++ fassion_dress/settings.py
@@ -126,6 +126,9 @@ USE_I18N = True
 USE_TZ = True
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
